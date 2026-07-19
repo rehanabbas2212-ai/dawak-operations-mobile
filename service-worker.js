2b85264aec65ac0734dca724052c2d6355a6d09c
rehanabbas2212-ai <rehanabbas2212@gmail.com>
2026-07-19 12:24:08 +0400

    Dawak proof photos and complete history v0.6.4

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='dawak-ops-v0.6.0';
+const CACHE='dawak-ops-v0.6.4';
 const ASSETS=['./','index.html','app.css','app.js','manifest.webmanifest','icon.svg','vendor/html5-qrcode.min.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
